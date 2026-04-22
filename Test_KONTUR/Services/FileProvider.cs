a1437b2c4f1426eaceb40b82cfe7cda2e604e69d
Максим Володько <twixkox@gmail.com>
2026-04-22 14:18:57 +0300

    fix

--- Test_KONTUR/Services/FileProvider.cs
+++ Test_KONTUR/Services/FileProvider.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Test_KONTUR.Models;
 
 namespace Test_KONTUR.Services
