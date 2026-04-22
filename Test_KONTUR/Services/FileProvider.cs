7d28b7091ab4a2387f2e7a7f38d290eee69ff00f
Максим Володько <twixkox@gmail.com>
2026-04-22 12:35:28 +0300

    add fileProvider

---
+++ Test_KONTUR/Services/FileProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_KONTUR.Models;
+
+namespace Test_KONTUR.Services
+{
+    public class FileProvider
+    {
+        public void ExportToCsv(List<Packet> packets, string filePath)
+        {
+            using (var writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine("Packet,Channel 1,Channel 2,Channel 3,Channel 4,Channel 5,Channel 6");
+
+                foreach (var packet in packets)
+                {
+                    writer.WriteLine($"{packet.PacketNumber}," +
+                        $"{packet.AvgChannel1}," +
+                        $"{packet.AvgChannel2}," +
+                        $"{packet.AvgChannel3}," +
+                        $"{packet.AvgChannel4}," +
+                        $"{packet.AvgChannel5}," +
+                        $"{packet.AvgChannel6}");
+                }
+            }
+        }
+    }
+}
