9e1e656a0fcdc4aab92676fe85ccc3c8e6a651a2
Максим Володько <twixkox@gmail.com>
2026-04-22 11:12:36 +0300

    add models

---
+++ Test_KONTUR/Models/DataBlock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_KONTUR.Models
+{
+    public class DataBlock
+    {
+        public int Channel1 { get; set; }
+        public int Channel2 { get; set; }
+        public int Channel3 { get; set; }
+        public int Channel4 { get; set; }
+        public int Channel5 { get; set; }
+        public int Channel6 { get; set; }
+    }
+}
---
+++ Test_KONTUR/Models/Packet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_KONTUR.Models
+{
+    public class Packet
+    {
+        public uint Size { get; set; }
+        public uint Type { get; set; }
+        public uint PacketNumber { get; set; }
+        public uint TimeMs { get; set; }
+        public List<DataBlock> DataBlocks { get; set; } = new List<DataBlock>();
+
+        public double AvgChannel1 => DataBlocks.Average(b => b.Channel1);
+        public double AvgChannel2 => DataBlocks.Average(b => b.Channel2);
+        public double AvgChannel3 => DataBlocks.Average(b => b.Channel3);
+        public double AvgChannel4 => DataBlocks.Average(b => b.Channel4);
+        public double AvgChannel5 => DataBlocks.Average(b => b.Channel5);
+        public double AvgChannel6 => DataBlocks.Average(b => b.Channel6);
+    }
+}
